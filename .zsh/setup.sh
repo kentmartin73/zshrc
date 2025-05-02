3bdee7e0b07b220ff13de48a4b4bc5752b10117e
Kent Martin <kent@kentsworld.com>
2025-05-02 22:28:17 +0200

    Typo

--- .zsh/setup.sh
+++ .zsh/setup.sh
@@ -101,5 +101,5 @@ echo -e "  2. Customize your local settings in: ${YELLOW}~/.zsh/local.zsh${NC}"
 echo
 echo -e "Your previous configuration has been backed up to: ${YELLOW}$BACKUP_DIR${NC}"
 echo
-echo -e "${YELLOW}Note: In any application that you use a shell (e.g. VS Code), you shoukd change the fond to \"MesloLGS Nerd Font\"${NC}"
+echo -e "${YELLOW}Note: In any application that you use a shell (e.g. VS Code), you shoukd change the font to \"MesloLGS Nerd Font\"${NC}"
 echo
