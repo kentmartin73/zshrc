a1950a0626da40f6f8b742a867f1ddcffbddd2a3
Kent Martin <kent@kentsworld.com>
2025-05-02 20:16:34 +0200

    Add status indicators: green tick for success, red cross with code for failure

--- .zsh/theme.zsh
+++ .zsh/theme.zsh
@@ -33,6 +33,21 @@ typeset -g POWERLEVEL9K_AWS_PROD_FOREGROUND=white
 typeset -g POWERLEVEL9K_AWS_DEFAULT_BACKGROUND=208
 typeset -g POWERLEVEL9K_AWS_DEFAULT_FOREGROUND=white
 
+# Status settings - show green tick for success, red cross with code for failure
+typeset -g POWERLEVEL9K_STATUS_OK=true
+typeset -g POWERLEVEL9K_STATUS_OK_VISUAL_IDENTIFIER_EXPANSION='✓'
+typeset -g POWERLEVEL9K_STATUS_OK_FOREGROUND=green
+typeset -g POWERLEVEL9K_STATUS_OK_BACKGROUND=0
+
+typeset -g POWERLEVEL9K_STATUS_ERROR=true
+typeset -g POWERLEVEL9K_STATUS_ERROR_VISUAL_IDENTIFIER_EXPANSION='✗'
+typeset -g POWERLEVEL9K_STATUS_ERROR_FOREGROUND=red
+typeset -g POWERLEVEL9K_STATUS_ERROR_BACKGROUND=0
+
+typeset -g POWERLEVEL9K_STATUS_VERBOSE=true
+typeset -g POWERLEVEL9K_STATUS_CROSS=true
+typeset -g POWERLEVEL9K_STATUS_SHOW_PIPESTATUS=true
+
 # Customize prompt appearance
 typeset -g POWERLEVEL9K_MODE=nerdfont-complete
 typeset -g POWERLEVEL9K_PROMPT_ON_NEWLINE=false
@@ -47,11 +62,6 @@ typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_from_right
 # VCS settings
 typeset -g POWERLEVEL9K_VCS_SHOW_SUBMODULE_DIRTY=false
 typeset -g POWERLEVEL9K_VCS_GIT_HOOKS=(vcs-detect-changes git-untracked git-aheadbehind git-stash git-remotebranch git-tagname)
-
-# Status settings
-typeset -g POWERLEVEL9K_STATUS_OK=false
-typeset -g POWERLEVEL9K_STATUS_ERROR_BACKGROUND=red
-typeset -g POWERLEVEL9K_STATUS_ERROR_FOREGROUND=white
 
 # Command execution time settings
 typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_THRESHOLD=3
